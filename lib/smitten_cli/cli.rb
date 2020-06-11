10c639beb44307f9b69f7287f1311a6ea8553687
eleemw <eleemw+codenews@gmail.com>
2020-06-11 16:52:21 -0700

    began stubbing out CLI

--- lib/smitten_cli/cli.rb
+++ lib/smitten_cli/cli.rb
@@ -1,3 +1,19 @@
 class SmittenCli::CLI
-    
+   
+    # Start Smitten CLI App
+    # Print greeting
+    # Get flavors from Smitten website
+    # Create new flavor objects & save
+    # Print numbered list of flavors
+    # Print menu options
+            # View list of flavors
+            # View description
+            # View ingredients
+            # Exit program
+    # Collect User input
+    # Print description of flavor User selected
+    # Print menu options    
+    # Collect User input
+
+
 end
